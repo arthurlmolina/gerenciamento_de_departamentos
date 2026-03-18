d6d0f23f03de0c2c636eb0ad778c850af2d364e7
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-18 20:10:32 -0300

    Initial commit

---
+++ src/main/java/com/example/gerenciamento_de_departamentos/GerenciamentoDeDepartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_departamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeDepartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeDepartamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_de_departamentos/GerenciamentoDeDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeDepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
