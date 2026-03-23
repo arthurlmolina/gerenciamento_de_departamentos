b41d45cd2c5769ee69f5a29fc473268222cb96b5
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-23 17:52:14 -0300

    model criada

---
+++ src/main/java/com/example/gerenciamento_de_departamentos/models/DepartamentoModel.java
@@ -0,0 +1,17 @@
+package com.example.gerenciamento_de_departamentos.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_departamento")
+public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeDepartamento")
+    private String nome;
+    @Column(name = "localizacaoDepartamento")
+    private String localizacao;
+    
+}
